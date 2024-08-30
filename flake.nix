469c4c7b73e3007ec0749ab51ce0695eacaa7674
Clément Patout <2178406+clempat@users.noreply.github.com>
2024-08-30 20:41:55 +0200

    refactor: try with helm chart

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
     in {
       devShells = forEachSupportedSystem ({ pkgs }: {
         default = pkgs.mkShell {
-          packages = with pkgs; [ terraform kubectl sops fluxcd go-task ];
+          packages = with pkgs; [ terraform kubectl sops fluxcd go-task kubernetes-helm ];
         };
       });
     };
